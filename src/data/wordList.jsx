1582f862fc04315aa162df9c43e5bf4d424a3a17
Leonardo Bezerra <leossbezerra@gmail.com>
2023-08-12 16:51:24 -0300

    updated css

--- src/data/wordList.jsx
+++ src/data/wordList.jsx
@@ -1,7 +1,8 @@
 export const wordsList = [
-	{id: 1, word: "UVA"			, tip: "Fruta"				, used: false},
-	{id: 2, word: "PERNA"		, tip: "Parte do corpo"		, used: false},
-	{id: 3, word: "PIANO"		, tip: "Instrumento"		, used: false},
-	{id: 4, word: "CPU"			, tip: "Computador"			, used: false},
-	{id: 5, word: "ALFACE"		, tip: "Vegetal"			, used: false}
+	{id: 1, word: "UVA"				, tip: "Fruta"				, used: false},
+	{id: 2, word: "PERNA"			, tip: "Parte do corpo"		, used: false},
+	{id: 3, word: "PIANO"			, tip: "Instrumento"		, used: false},
+	{id: 4, word: "CPU"				, tip: "Computador"			, used: false},
+	{id: 5, word: "ALFACE"			, tip: "Vegetal"			, used: false},
+	{id: 6, word: "PARALELEPIPEDO"	, tip: "Geometria"			, used: false}
 ]
